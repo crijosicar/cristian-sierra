7bb472a0d62a4a4ec036ac0f7ddd06d0cd54aea6
Cristian Sierra <crijosicar@hotmail.com>
2022-05-03 18:21:38 -0400

    Added Google Analytics service

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Script from "next/script";
 
 interface LayoutPage {
   [index: string]: any;
@@ -8,6 +9,19 @@ interface LayoutPage {
 const Layout = ({ children }: LayoutPage) => {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XYZVCCFKML"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-XYZVCCFKML');
+        `}
+      </Script>
       <Navbar />
       <main>{children}</main>
       <Footer />
